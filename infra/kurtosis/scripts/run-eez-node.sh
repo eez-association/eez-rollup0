1f6cfa13b2df5e72edec22bf6b42f8e025b0d63b
Chetna Miittal <chetna.mittal125@gmail.com>
2026-07-01 16:43:14 +0530

    chore: script changes

--- infra/kurtosis/scripts/run-eez-node.sh
+++ infra/kurtosis/scripts/run-eez-node.sh
@@ -26,10 +26,17 @@ set +a
 mkdir -p "$EEZ_L2_DATADIR"
 EEZ_L1_EMBEDDED="${EEZ_L1_EMBEDDED:-0}"
 
+L2_P2P_PORT="${EEZ_L2_P2P_PORT:-30640}"
+L2_DISCOVERY_PORT="${EEZ_L2_DISCOVERY_PORT:-$L2_P2P_PORT}"
+L2_DISCOVERY_V5_PORT="${EEZ_L2_DISCOVERY_V5_PORT:-$((L2_P2P_PORT + 1))}"
+
 exec cargo run -p eez-node -- node \
     --chain="$EEZ_L2_GENESIS_PATH" \
     --datadir="$EEZ_L2_DATADIR" \
     --http --http.addr=0.0.0.0 --http.port="${EEZ_L2_HTTP_PORT:-18688}" \
     --http.api=eth,net,web3 \
     --authrpc.addr=127.0.0.1 --authrpc.port="${EEZ_L2_AUTH_PORT:-18684}" \
+    --port="$L2_P2P_PORT" \
+    --discovery.port="$L2_DISCOVERY_PORT" \
+    --discovery.v5.port="$L2_DISCOVERY_V5_PORT" \
     --ipcdisable --disable-discovery
